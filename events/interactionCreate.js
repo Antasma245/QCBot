ab0838036c9ea2c226fa459da56deae0ba75bda8
Antasma245 <144015963+Antasma245@users.noreply.github.com>
2024-05-04 15:44:08 +0200

    Added basic event listeners

---
+++ events/interactionCreate.js
@@ -0,0 +1,40 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const env = require('dotenv').config();
+const botAdminId = process.env.botAdminId;
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`[ERROR] No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (interactionError) {
+			console.error(`[ERROR] New error report! Occured on ${new Date().toUTCString()} while executing '/${interaction.commandName}'`);
+			console.log(interactionError);
+
+			try {
+				const botAdmin = await interaction.client.users.fetch(botAdminId);
+				await botAdmin.send({
+					embeds: [ new EmbedBuilder()
+						.setColor(`6ba4b8`)
+						.setTitle(`New error report!`)
+						.setDescription(`Occured on ${new Date().toUTCString()} while executing '/${interaction.commandName}'\n\`\`\`\n${interactionError}\n\`\`\``) ],
+					flags: [ 4096 ] // Silent message flag
+				});
+
+				console.log(`[INFO] A copy of the previous message has been sent to ${botAdmin.displayName}`);
+			} catch (dmError) {
+				console.warn(`[WARNING] Failed to send a copy of the previous message to the bot administrator`);
+				console.log(dmError);
+			}
+		}
+	}
+};

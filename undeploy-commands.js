96ece6ea6a401b0319969fe13f41e1086eaf7bbb
Antasma245 <144015963+Antasma245@users.noreply.github.com>
2024-05-04 10:21:04 +0200

    Added basic bot files

---
+++ undeploy-commands.js
@@ -0,0 +1,10 @@
+const { REST, Routes } = require('discord.js');
+const env = require('dotenv').config();
+const botToken = process.env.botToken;
+const botClientId = process.env.botClientId;
+
+const rest = new REST().setToken(botToken);
+
+rest.put(Routes.applicationCommands(botClientId), { body: [] })
+	.then(() => console.log('Successfully removed all application (/) commands.'))
+	.catch(console.error);
